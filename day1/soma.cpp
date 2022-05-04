9d30649711dfedd24a7bcae3977be8d30703d28f
HiimHotta <daniel.y.hotta@gmail.com>
2022-05-04 13:25:40 -0300

    Create soma.cpp

---
+++ day1/soma.cpp
@@ -0,0 +1,18 @@
+//link: https://neps.academy/br/exercise/84
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+
+    int x, y, s;
+    
+    cin >> x; cin >> y;
+    
+    s = x + y;
+    
+    cout << s << endl;
+ 
+    return 0;
+}
